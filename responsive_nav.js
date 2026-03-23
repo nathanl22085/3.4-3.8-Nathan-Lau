da99bf1a1ed8fcd6f1e875539a9aca652f0d6edc
nathanl22085 <nathanl.22085@obhs.school.nz>
2026-03-24 07:46:27 +1300

    Responsive nav for phone view

---
+++ responsive_nav.js
@@ -0,0 +1,10 @@
+// This is my responsive navigation changing into phone view
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
