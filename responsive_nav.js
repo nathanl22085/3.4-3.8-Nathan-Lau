0ef798355964205a954713b7b638372072e04eb6
nathanl22085 <nathanl.22085@obhs.school.nz>
2026-04-01 10:33:34 +1300

    rewrote my topnav for visual effects in phone view that being a sliding animation down. also added to my javascript to make it so my h1 tags that sit on my top parallax images disappear and reappear upon opening and closing the hamburger menu.

--- responsive_nav.js
+++ responsive_nav.js
@@ -1,10 +1,13 @@
-// This is my responsive navigation changing into phone view
-
 function myFunction() {
   var x = document.getElementById("myTopnav");
+  // Target the h1 inside the parallax text containers
+  var parallaxH1 = document.querySelector('.parallax_text_art_of_tennis h1, .parallax_text_how_to_play h1');
+
   if (x.className === "topnav") {
     x.className += " responsive";
+    if (parallaxH1) parallaxH1.classList.add("hidden-h1");
   } else {
     x.className = "topnav";
+    if (parallaxH1) parallaxH1.classList.remove("hidden-h1");
   }
 }
