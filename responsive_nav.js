55499be46812b54589af965caf95166e827fcb74
nathanl22085 <nathanl.22085@obhs.school.nz>
2026-04-29 11:04:14 +1200

    moved my arrows onto my hero parallax images, resized them to fit the entire screen, put my accordions above my clickable images, sized down my p tag text. resize and edited my topnav a tags and sized down my footer text

--- responsive_nav.js
+++ responsive_nav.js
@@ -1,13 +1,19 @@
 function myFunction() {
   var x = document.getElementById("myTopnav");
-  // Target the h1 inside the parallax text containers
   var parallaxH1 = document.querySelector('.parallax_text_art_of_tennis h1, .parallax_text_how_to_play h1, .parallax_text_grand_slam h1, .parallax_text_big_three');
+  
+  // Target the arrow container
+  var arrow = document.querySelector('.arrow_down');
 
   if (x.className === "topnav") {
     x.className += " responsive";
     if (parallaxH1) parallaxH1.classList.add("hidden-h1");
+    // Hide the arrow
+    if (arrow) arrow.classList.add("hidden-arrow");
   } else {
     x.className = "topnav";
     if (parallaxH1) parallaxH1.classList.remove("hidden-h1");
+    // Show the arrow again
+    if (arrow) arrow.classList.remove("hidden-arrow");
   }
 }
